7a5e20a1c6d9367220a2ac6504e81d34e7d3969f
harsh028-raj <74181011+harsh028-raj@users.noreply.github.com>
2025-07-14 11:56:16 +0530

    Update backend.tf
    updated use_lockfile   = true  # replaces 'dynamodb_table'

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "terraform-state-harsh2812"
     key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
+    use_lockfile   = true
     encrypt        = true
   }
 }
